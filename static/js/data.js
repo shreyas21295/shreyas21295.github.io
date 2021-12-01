4c35d9128641fdb08f918df5c5799ce02ef8a331
shreyas21295 <84985069+shreyas21295@users.noreply.github.com>
2021-12-01 01:10:41 -0500

    Update data.js

--- static/js/data.js
+++ static/js/data.js
@@ -5,6 +5,16 @@ data = [
         "URLs": ['http://www.mit.edu/~9.54/fall14/slides/Class13.pdf','https://cs.nyu.edu/~mohri/mlu/mlu_lecture_1.pdf', 'https://mrcet.com/downloads/digital_notes/CSE/IV%20Year/MACHINE%20LEARNING(R17A0534).pdf', 'https://pdfs.semanticscholar.org/1742/1f86f34998d3125972742bae20e863de0812.pdf', 'https://projects.iq.harvard.edu/gov2001/files/discov.pdf']
     },
     {
+        "Topic": "unsupervised learning",
+        "Difficuly": "easy",
+        "URLs": ['http://research.iac.es/winterschool/2018/media/summaries/iac_winter_syllabus_MHC.pdf', 'http://users.metu.edu.tr/alatan/Courses/EE583LECTURES/pr05.pdf', 'http://www.mit.edu/~9.54/fall14/slides/Class13.pdf', 'https://cs.nyu.edu/~mohri/mlu/mlu_lecture_1.pdf', 'https://mrcet.com/downloads/digital_notes/CSE/IV%20Year/MACHINE%20LEARNING(R17A0534).pdf']
+    },
+    {
+        "Topic": "unsupervised learning",
+        "Difficuly": "hard",
+        "URLs": ['https://www.robots.ox.ac.uk/~az/lectures/ml/lect1.pdf', 'https://www.cs.toronto.edu/~lczhang/321/notes/notes01.pdf', 'https://www.andrew.cmu.edu/user/achoulde/95791/lectures/lecture07/lecture07_95791.pdf', 'https://projects.iq.harvard.edu/gov2001/files/discov.pdf', 'https://pdfs.semanticscholar.org/1742/1f86f34998d3125972742bae20e863de0812.pdf']
+    },
+    {
         "Topic": "data science",
         "Difficuly": "easy",
         "URLs": ['http://math.ecnu.edu.cn/~lfzhou/seminar/[Joel_Grus]_Data_Science_from_Scratch_First_Princ.pdf', 'http://www.cse.chalmers.se/~richajo/dit862/L1/l1.pdf', 'https://cims.nyu.edu/~cfgranda/pages/stuff/probability_stats_for_DS.pdf', 'https://courses.helsinki.fi/sites/default/files/course-material/4537296/IntroDS-01.pdf', 'https://engineering.lehigh.edu/sites/engineering.lehigh.edu/files/ISE444-syllabus%20%281%29.pdf']
