5336c86d4ac9f6718df3e704ba3c5f2e86f0a5c6
shreyas21295 <84985069+shreyas21295@users.noreply.github.com>
2021-12-13 19:37:21 -0500

    Update data.js

--- static/js/data.js
+++ static/js/data.js
@@ -1,122 +1,134 @@
 data = [
     {
-        "Topic": "unsupervised learning",
-        "Difficuly": "medium",
-        "URLs": ['http://www.mit.edu/~9.54/fall14/slides/Class13.pdf','https://cs.nyu.edu/~mohri/mlu/mlu_lecture_1.pdf', 'https://mrcet.com/downloads/digital_notes/CSE/IV%20Year/MACHINE%20LEARNING(R17A0534).pdf', 'https://pdfs.semanticscholar.org/1742/1f86f34998d3125972742bae20e863de0812.pdf', 'https://projects.iq.harvard.edu/gov2001/files/discov.pdf']
+        "DifficultyLevel": "easy",
+        "URLs": [
+            "https://www.dgp.toronto.edu/~hertzman/418notes.pdf",
+            "http://www.cim.mcgill.ca/~langer/557/lecturenotesCOMP557.pdf",
+            "https://www.cl.cam.ac.uk/teaching/1112/CompGraph/notes.pdf",
+            "https://engineering.nyu.edu/sites/default/files/2020-10/CS-UY%204533%20Interactive%20Computer%20Graphics.pdf",
+            "https://images.nvidia.com/aem-dam/en-zz/Solutions/data-center/nvidia-ampere-architecture-whitepaper.pdf"
+        ],
+        "Topic": "GPU sharing"
     },
     {
-        "Topic": "unsupervised learning",
-        "Difficuly": "easy",
-        "URLs": ['http://research.iac.es/winterschool/2018/media/summaries/iac_winter_syllabus_MHC.pdf', 'http://users.metu.edu.tr/alatan/Courses/EE583LECTURES/pr05.pdf', 'http://www.mit.edu/~9.54/fall14/slides/Class13.pdf', 'https://cs.nyu.edu/~mohri/mlu/mlu_lecture_1.pdf', 'https://mrcet.com/downloads/digital_notes/CSE/IV%20Year/MACHINE%20LEARNING(R17A0534).pdf']
+        "DifficultyLevel": "medium",
+        "URLs": [
+            "https://engineering.nyu.edu/sites/default/files/2020-10/CS-UY%204533%20Interactive%20Computer%20Graphics.pdf",
+            "https://images.nvidia.com/aem-dam/en-zz/Solutions/data-center/nvidia-ampere-architecture-whitepaper.pdf",
+            "https://passlab.github.io/CSCE569/notes/lecture_GPUArchCUDA01.pdf",
+            "http://www.cs.cmu.edu/afs/cs/academic/class/15418-f18/www/lectures/gpu-handout.pdf",
+            "https://inst.eecs.berkeley.edu/~cs152/fa16/lectures/L16-GPU.pdf"
+        ],
+        "Topic": "GPU sharing"
     },
     {
-        "Topic": "unsupervised learning",
-        "Difficuly": "hard",
-        "URLs": ['https://www.robots.ox.ac.uk/~az/lectures/ml/lect1.pdf', 'https://www.cs.toronto.edu/~lczhang/321/notes/notes01.pdf', 'https://www.andrew.cmu.edu/user/achoulde/95791/lectures/lecture07/lecture07_95791.pdf', 'https://projects.iq.harvard.edu/gov2001/files/discov.pdf', 'https://pdfs.semanticscholar.org/1742/1f86f34998d3125972742bae20e863de0812.pdf']
+        "DifficultyLevel": "hard",
+        "URLs": [
+            "http://personal.ee.surrey.ac.uk/Personal/J.Collomosse/pubs/cm20219.pdf",
+            "http://web.eece.maine.edu/~vweaver/classes/ece571_2018s/ece571_lec24.pdf",
+            "https://www.cs.cornell.edu/courses/cs4787/2019sp/notes/lecture21.pdf",
+            "https://inst.eecs.berkeley.edu/~cs152/fa16/lectures/L16-GPU.pdf",
+            "http://www.cs.cmu.edu/afs/cs/academic/class/15418-f18/www/lectures/gpu-handout.pdf"
+        ],
+        "Topic": "GPU sharing"
     },
     {
-        "Topic": "data science",
-        "Difficuly": "easy",
-        "URLs": ['http://math.ecnu.edu.cn/~lfzhou/seminar/[Joel_Grus]_Data_Science_from_Scratch_First_Princ.pdf', 'http://www.cse.chalmers.se/~richajo/dit862/L1/l1.pdf', 'https://cims.nyu.edu/~cfgranda/pages/stuff/probability_stats_for_DS.pdf', 'https://courses.helsinki.fi/sites/default/files/course-material/4537296/IntroDS-01.pdf', 'https://engineering.lehigh.edu/sites/engineering.lehigh.edu/files/ISE444-syllabus%20%281%29.pdf']
+        "DifficultyLevel": "easy",
+        "URLs": [
+            "https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-867-machine-learning-fall-2006/lecture-notes/lec16.pdf",
+            "https://www.cs.toronto.edu/~rgrosse/courses/csc411_f18/slides/lec16-slides.pdf",
+            "https://www.math.kth.se/matstat/gru/Statistical%20inference/Lecture8.pdf",
+            "https://courses.cs.washington.edu/courses/cse527/06au/slides/lec04-mle-em-notes.pdf",
+            "http://cs229.stanford.edu/notes2021fall/cs229-notes8.pdf"
+        ],
+        "Topic": "expectation maximization"
     },
     {
-        "Topic": "data science",
-        "Difficuly": "medium",
-        "URLs": ['https://cims.nyu.edu/~cfgranda/pages/stuff/probability_stats_for_DS.pdf', 'https://courses.helsinki.fi/sites/default/files/course-material/4537296/IntroDS-01.pdf', 'https://engineering.lehigh.edu/sites/engineering.lehigh.edu/files/ISE444-syllabus%20%281%29.pdf', 'https://homepage.cs.uri.edu/faculty/hamel/courses/2018/spring2018/csc310/lecture-notes/01-What-is-Data-Science.pdf', 'https://www.iare.ac.in/sites/default/files/lecture_notes/APA%20Lecture%20notes.pdf']
+        "DifficultyLevel": "medium",
+        "URLs": [
+            "https://www.math.kth.se/matstat/gru/Statistical%20inference/Lecture8.pdf",
+            "https://courses.cs.washington.edu/courses/cse527/06au/slides/lec04-mle-em-notes.pdf",
+            "http://cs229.stanford.edu/notes2021fall/cs229-notes8.pdf",
+            "http://www.lx.it.pt/~mtf/Figueiredo_EM_Algorithm.pdf",
+            "https://home.ttic.edu/~dmcallester/ttic101-07/lectures/em/em.pdf"
+        ],
+        "Topic": "expectation maximization"
     },
     {
-        "Topic": "data science",
-        "Difficuly": "hard",
-        "URLs": ['https://www.randomsystems-cdt.ac.uk/sites/default/files/randomsystems/documents/media/foundationsofdatascience.pdf', 'https://www.mie.utoronto.ca/wp-content/uploads/2019/08/MIE1624H-Course-Outline-Winter-2020.pdf', 'https://www.leanderband.org/wp-content/uploads/formidable/17/advanced-statistical-inference-lecture-notes.pdf', 'https://www.iare.actes/default/files/lecture_notes/.in/siAPA%20Lecture%20notes.pdf', 'https://homepage.cs.uri.edu/faculty/hamel/courses/2018/spring2018/csc310/lecture-notes/01-What-is-Data-Science.pdf']
-    },    
-    {
-        "Topic": "machine learning",
-        "Difficuly": "easy",
-        "URLs": ['https://mrcet.com/downloads/digital_notes/CSE/IV%20Year/MACHINE%20LEARNING(R17A0534).pdf', 'https://sebastianraschka.com/pdf/lecture-notes/stat479fs18/01_ml-overview_notes.pdf', 'https://www.dgp.toronto.edu/~hertzman/411notes.pdf', 'https://www.robots.ox.ac.uk/~az/lectures/ml/lect1.pdf', 'https://faculty.ucmerced.edu/mcarreira-perpinan/teaching/CSE176/lecturenotes.pdf']
-    },   
-    {
-        "Topic": "machine learning",
-        "Difficuly": "medium",
-        "URLs": ['https://www.dgp.toronto.edu/~hertzman/411notes.pdf', 'https://www.robots.ox.ac.uk/~az/lectures/ml/lect1.pdf', 'https://faculty.ucmerced.edu/mcarreira-perpinan/teaching/CSE176/lecturenotes.pdf', 'https://cs.nyu.edu/~mohri/mlu/mlu_lecture_1.pdf', 'http://www.stat.sinica.edu.tw/syhuang/kern_stat_toolbox/lecture-notes.pdf']
+        "DifficultyLevel": "hard",
+        "URLs": [
+            "https://engineering.purdue.edu/ChanGroup/ECE645Notes/StudentLecture10.pdf",
+            "https://www.cs.cmu.edu/~epxing/Class/10708-17/notes-17/10708-scribe-lecture8.pdf",
+            "https://www.cs.cmu.edu/~psarkar/sds383c_16/lecture18.pdf",
+            "https://home.ttic.edu/~dmcallester/ttic101-07/lectures/em/em.pdf",
+            "http://www.lx.it.pt/~mtf/Figueiredo_EM_Algorithm.pdf"
+        ],
+        "Topic": "expectation maximization"
     },
     {
-        "Topic": "machine learning",
-        "Difficuly": "hard",
-        "URLs": ['http://www.cs.ccsu.edu/~markov/ccsu_courses/lnml.pdf', 'https://news.vidyaacademy.ac.in/wp-content/uploads/2018/10/NotesOnMachineLearningForBTech-1.pdf', 'https://www.ai.rug.nl/minds/uploads/LN_ML_RUG.pdf', 'http://www.stat.sinica.edu.tw/syhuang/kern_stat_toolbox/lecture-notes.pdf', 'https://cs.nyu.edu/~mohri/mlu/mlu_lecture_1.pdf']
+        "DifficultyLevel": "easy",
+        "URLs": [
+            "https://ocw.mit.edu/courses/sloan-school-of-management/15-097-prediction-machine-learning-and-statistics-spring-2012/lecture-notes/MIT15_097S12_lec08.pdf",
+            "https://www4.stat.ncsu.edu/~lu/ST7901/lecture%20notes/2019Lect21_tree.pdf",
+            "https://www.cs.princeton.edu/courses/archive/fall16/cos402/lectures/402-lec2.pdf",
+            "http://www.cs.cmu.edu/afs/cs/project/theo-20/www/mlbook/ch3.pdf",
+            "https://www.andrew.cmu.edu/user/achoulde/95791/lectures/lecture05/lecture05_95791.pdf"
+        ],
+        "Topic": "Decision Trees"
     },
     {
-        "Topic": "cross validation",
-        "Difficuly": "easy",
-        "URLs": ['http://courses.ieor.berkeley.edu/ieor165/lecture_notes/ieor165_lec9.pdf', 'http://www.stat.cmu.edu/~ryantibs/datamining/lectures/18-val1.pdf', 'http://www2.stat.duke.edu/~rcs46/lectures_2017/05-resample/05-cv.pdf', 'https://ocw.mit.edu/courses/health-sciences-and-technology/hst-951j-medical-decision-support-fall-2005/lecture-notes/hst951_6.pdf', 'https://www.cmpe.boun.edu.tr/~ethem/i2ml2e/2e_v1-0/i2ml2e-chap19-v1-0.pdf']
+        "DifficultyLevel": "medium",
+        "URLs": [
+            "https://www4.stat.ncsu.edu/~lu/ST7901/lecture%20notes/2019Lect21_tree.pdf",
+            "https://www.cs.princeton.edu/courses/archive/fall16/cos402/lectures/402-lec2.pdf",
+            "http://www.cs.cmu.edu/afs/cs/project/theo-20/www/mlbook/ch3.pdf",
+            "https://www.andrew.cmu.edu/user/achoulde/95791/lectures/lecture05/lecture05_95791.pdf",
+            "https://cogsys.uni-bamberg.de/pre08/teaching/ss05/ml/slides/cogsysII-3.pdf"
+        ],
+        "Topic": "Decision Trees"
     },
     {
-        "Topic": "cross validation",
-        "Difficuly": "medium",
-        "URLs": ['http://www.stat.cmu.edu/~ryantibs/datamining/lectures/18-val1.pdf', 'http://www2.stat.duke.edu/~rcs46/lectures_2017/05-resample/05-cv.pdf', 'https://ocw.mit.edu/courses/health-sciences-and-technology/hst-951j-medical-decision-support-fall-2005/lecture-notes/hst951_6.pdf', 'https://www.cmpe.boun.edu.tr/~ethem/i2ml2e/2e_v1-0/i2ml2e-chap19-v1-0.pdf', 'https://www.cs.tau.ac.il/~nin/Courses/NC05/pr_l13.pdf']
+        "DifficultyLevel": "hard",
+        "URLs": [
+            "https://www.cs.toronto.edu/~urtasun/courses/CSC411_Fall16/06_trees_handout.pdf",
+            "https://cogsys.uni-bamberg.de/pre08/teaching/ss05/ml/slides/cogsysII-3.pdf",
+            "https://www.andrew.cmu.edu/user/achoulde/95791/lectures/lecture05/lecture05_95791.pdf",
+            "http://www.cs.cmu.edu/afs/cs/project/theo-20/www/mlbook/ch3.pdf",
+            "https://www.cs.princeton.edu/courses/archive/fall16/cos402/lectures/402-lec2.pdf"
+        ],
+        "Topic": "Decision Trees"
     },
     {
-        "Topic": "cross validation",
-        "Difficuly": "hard",
-        "URLs": ['https://www3.cs.stonybrook.edu/~cse352/L7classification2.pdf', 'https://www.cs.tau.ac.il/~nin/Courses/NC05/pr_l13.pdf', 'https://www.cmpe.boun.edu.tr/~ethem/i2ml2e/2e_v1-0/i2ml2e-chap19-v1-0.pdf', 'https://ocw.mit.edu/courses/health-sciences-and-technology/hst-951j-medical-decision-support-fall-2005/lecture-notes/hst951_6.pdf', 'http://www2.stat.duke.edu/~rcs46/lectures_2017/05-resample/05-cv.pdf']
+        "DifficultyLevel": "easy",
+        "URLs": [
+            "https://www.iro.umontreal.ca/~nie/IFT6255/Books/Learning-to-rank.pdf",
+            "http://www.cs.nyu.edu/~mohri/mlu/mlu_lecture_16.pdf",
+            "https://icml.cc/Conferences/2015/wp-content/uploads/2015/06/icml_ranking.pdf",
+            "http://times.cs.uiuc.edu/course/598f14/l2r.pdf",
+            "https://www.hangli-hl.com/uploads/3/4/4/6/34465961/learning_to_rank.pdf"
+        ],
+        "Topic": "learning to rank"
     },
     {
-        "Topic": "tectonic plates",
-        "Difficuly": "easy",
-        "URLs": ['http://geofaculty.uwyo.edu/dueker/GeophysicsClass/Plate_Tectonics%20WATT%20SLIDES.pdf', 'http://ivis.eps.pitt.edu/courses/geol0820/lectures/5.pdf', 'http://www.geo.cornell.edu/eas/education/course/descr/EAS220/2009_Fall_Jason_Chao/lectures/2200_09Lecture07.pdf', 'http://www.geo.hunter.cuny.edu/~fbuon/GEOL_231/Lectures/Tectonic%20Landforms.pdf', 'https://ocw.mit.edu/courses/earth-atmospheric-and-planetary-sciences/12-001-introduction-to-geology-fall-2013/lecture-notes-and-slides/MIT12_001F13_Lec13-15Notes.pdf']
+        "DifficultyLevel": "medium",
+        "URLs": [
+            "https://icml.cc/Conferences/2015/wp-content/uploads/2015/06/icml_ranking.pdf",
+            "http://times.cs.uiuc.edu/course/598f14/l2r.pdf",
+            "https://www.hangli-hl.com/uploads/3/4/4/6/34465961/learning_to_rank.pdf",
+            "http://dept.stat.lsa.umich.edu/~tewaria/theses/Sougata_Chaudhuri_Thesis_2016.pdf",
+            "https://web.stanford.edu/class/cs276/19handouts/lecture15-learning-ranking-1per.pdf"
+        ],
+        "Topic": "learning to rank"
     },
     {
-        "Topic": "tectonic plates",
-        "Difficuly": "medium",
-        "URLs": ['http://www.geo.cornell.edu/eas/education/course/descr/EAS220/2009_Fall_Jason_Chao/lectures/2200_09Lecture07.pdf', 'http://www.geo.hunter.cuny.edu/~fbuon/GEOL_231/Lectures/Tectonic%20Landforms.pdf', 'https://ocw.mit.edu/courses/earth-atmospheric-and-planetary-sciences/12-001-introduction-to-geology-fall-2013/lecture-notes-and-slides/MIT12_001F13_Lec13-15Notes.pdf', 'https://web.gccaz.edu/academicdepartments/physicalscience/engweb/engftp/GLG101/Lecture1_intro_pltec.pdf', 'https://www.kean.edu/~csmart/Observing/05.%20Plate%20tectonics.pdf']
-    },
-    {
-        "Topic": "tectonic plates",
-        "Difficuly": "hard",
-        "URLs": ['https://www.southalabama.edu/geology/haywick/GY111/111-2.pdf', 'https://www.ldisd.net/cms/lib5/TX01817232/Centricity/Domain/218/Plate%20tectonics%20notes_2016.pdf', 'https://www.lakeheadu.ca/sites/default/files/uploads/54/Geology%203015%20Week%204b%20Lecture.pdf', 'https://www.kean.edu/~csmart/Observing/05.%20Plate%20tectonics.pdf', 'https://web.gccaz.edu/academicdepartments/physicalscience/engweb/engftp/GLG101/Lecture1_intro_pltec.pdf']
-    },
-    {
-        "Topic": "python",
-        "Difficuly": "easy",
-        "URLs": ['http://www.cs.iit.edu/~cs560/fall_2012/week6-10/CS4RK(WeekNine).pdf', 'https://bratliservice.eu/doc/PythonNotesForProfessionals.pdf', 'https://bugs.python.org/file47781/Tutorial_EDIT.pdf', 'https://geosci.uchicago.edu/~rtp1/PythonSupport/PythonNotes.pdf', 'https://mrcet.com/downloads/digital_notes/CSE/III%20Year/PYTHON%20PROGRAMMING%20NOTES.pdf']
-    },
-    {
-        "Topic": "python",
-        "Difficuly": "medium",
-        "URLs": ['https://bratliservice.eu/doc/PythonNotesForProfessionals.pdf', 'https://bugs.python.org/file47781/Tutorial_EDIT.pdf', 'https://geosci.uchicago.edu/~rtp1/PythonSupport/PythonNotes.pdf', 'https://mrcet.com/downloads/digital_notes/CSE/III%20Year/PYTHON%20PROGRAMMING%20NOTES.pdf', 'https://www.ast.cam.ac.uk/~rcooke/python/UCS_python-intro_notes.pdf']
-    },
-    {
-        "Topic": "python",
-        "Difficuly": "hard",
-        "URLs": ['https://www.stat.berkeley.edu/~spector/python.pdf', 'https://www.iare.ac.in/sites/default/files/IARE_OOPS_Lecture%20Notes.pdf', 'https://www.ast.cam.ac.uk/~rcooke/python/UCS_python-intro_notes.pdf', 'https://mrcet.com/downloads/digital_notes/CSE/III%20Year/PYTHON%20PROGRAMMING%20NOTES.pdf', 'https://geosci.uchicago.edu/~rtp1/PythonSupport/PythonNotes.pdf']
-    },
-    {
-        "Topic": "application programming interface",
-        "Difficuly": "easy",
-        "URLs": ['https://idc9.github.io/stor390/slides/API.pdf\n', 'https://jolt.law.harvard.edu/assets/articlePDFs/v32/32HarvJLTech235.pdf\n', 'https://publications.jrc.ec.europa.eu/repository/bitstream/JRC118082/jrc118082_api-landscape-standards.pdf', 'https://resources.saylor.org/wwwresources/archived/site/wp-content/uploads/2013/02/CS101-1.3.5.3-Java-Application-Programming-Interface-API-FINAL.pdf', 'https://www.basicknowledge101.com/pdf/km/Application%20programming%20interface.pdf']
-    },
-    {
-        "Topic": "application programming interface",
-        "Difficuly": "medium",
-        "URLs": ['https://jolt.law.harvard.edu/assets/articlePDFs/v32/32HarvJLTech235.pdf', 'https://publications.jrc.ec.europa.eu/repository/bitstream/JRC118082/jrc118082_api-landscape-standards.pdf', 'https://resources.saylor.org/wwwresources/archived/site/wp-content/uploads/2013/02/CS101-1.3.5.3-Java-Application-Programming-Interface-API-FINAL.pdf', 'https://www.basicknowledge101.com/pdf/km/Application%20programming%20interface.pdf', 'https://www.godigital.co.kr/wp-content/uploads/formidable/2/internet-application-programming-lecture-notes.pdf']
-    },
-    {
-        "Topic": "application programming interface",
-        "Difficuly": "hard",
-        "URLs": ['https://www.w3.org/2004/02/mmi-workshop/vanderdonckt-louvain.pdf', 'https://www.iare.ac.in/sites/default/files/lecture_notes/MAD%20Lecture%20Notes.pdf', 'https://www.godigital.co.kr/wp-content/uploads/formidable/2/internet-application-programming-lecture-notes.pdf', 'https://www.basicknowledge101.com/pdf/km/Application%20programming%20interface.pdf', 'https://resources.saylor.org/wwwresources/archived/site/wp-content/uploads/2013/02/CS101-1.3.5.3-Java-Application-Programming-Interface-API-FINAL.pdf']
-    },
-    {
-        "Topic": "trigonometry",
-        "Difficuly": "easy",
-        "URLs": ['http://math.oit.edu/~paulr/Texts/notes_M112.pdf', 'http://www.mcclendonmath.com/m120/m120coursenotes.pdf', 'https://assets.openstax.org/oscms-prodcms/media/documents/OpenStax_Algebra__Trigonometry_Recorded_Lectures_and_Notes_for_Precalculus.pdf', 'https://jmahaffy.sdsu.edu/courses/f11/math122/beamer_lectures/trig.pdf', 'https://math.uchicago.edu/~cstaats/Charles_Staats_III/Notes_and_papers_files/Winter2012.pdf']
-    },
-    {
-        "Topic": "trigonometry",
-        "Difficuly": "medium",
-        "URLs": ['https://assets.openstax.org/oscms-prodcms/media/documents/OpenStax_Algebra__Trigonometry_Recorded_Lectures_and_Notes_for_Precalculus.pdf', 'https://jmahaffy.sdsu.edu/courses/f11/math122/beamer_lectures/trig.pdf', 'https://math.uchicago.edu/~cstaats/Charles_Staats_III/Notes_and_papers_files/Winter2012.pdf', 'https://orion.math.iastate.edu/butler/PDF/trig_notes.pdf', 'https://users.math.msu.edu/users/systeven/MTH114synotes.pdf']
-    },
-    {
-        "Topic": "trigonometry",
-        "Difficuly": "hard",
-        "URLs": ['https://www.pnw.edu/wp-content/uploads/2020/03/Lecture-Notes-9-1-2.pdf', 'https://www.govst.edu/uploadedFiles/Academics/Colleges_and_Programs/CAS/Trigonometry_Short_Course_Tutorial_Lauren_Johnson.pdf', 'https://users.math.msu.edu/users/systeven/MTH114synotes.pdf', 'https://orion.math.iastate.edu/butler/PDF/trig_notes.pdf', 'https://math.uchicago.edu/~cstaats/Charles_Staats_III/Notes_and_papers_files/Winter2012.pdf']
-    },
-    ];
+        "DifficultyLevel": "hard",
+        "URLs": [
+            "http://didawiki.di.unipi.it/lib/exe/fetch.php/magistraleinformatica/ir/ir13/1_-_learning_to_rank.pdf",
+            "https://www.utdallas.edu/~nrr150130/cs6375/2015fa/lects/Lecture_26_Ranking.pdf",
+            "https://www.cs.cornell.edu/people/tj/publications/radlinski_joachims_05a.pdf",
+            "https://web.stanford.edu/class/cs276/19handouts/lecture15-learning-ranking-1per.pdf",
+            "http://dept.stat.lsa.umich.edu/~tewaria/theses/Sougata_Chaudhuri_Thesis_2016.pdf"
+        ],
+        "Topic": "learning to rank"
+    }
+];
